c3add80e4a3040af5424bd25f2914922f60082ce
apple <apple@apple-MacBook-Pro.local>
2019-11-13 16:48:40 +0300

    Instagram clone with firebase

---
+++ InstagramCloneFirebase/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  InstagramCloneFirebase
+//
+//  Created by apple on 12.11.2019.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ InstagramCloneFirebase/UploadViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UploadViewController.swift
+//  InstagramCloneFirebase
+//
+//  Created by apple on 12.11.2019.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
